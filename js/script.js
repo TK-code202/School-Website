aa094221f2c0a05c08a1ea00dd17856dcb837a9f
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-06-07 22:29:00 +0100

    add submit function to script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
     $(".fa-bars").click(function() {
         $("#mobile-nav").toggle(1000);
     });
+
+
+    //Enrollment Page
+    $(".print_button").click(function() {
+        window.print();
+        return false;
+    });
+
+    $("#enrollmentCard").submit(function(event) {
+        $("#enrollmentCard").hide();
+        $(".response_card").show();
+        event.preventDefault();
+    });
 });
